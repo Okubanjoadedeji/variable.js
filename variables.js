ef587278cef3e24607b8c067109a7473db32d969
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-10-29 19:03:15 +0100

    variable

---
+++ variables.js
@@ -0,0 +1,24 @@
+let variable2 = 4;
+console.log(variable2);
+//console.log(2);
+
+//let calculation = 64 + 16;
+//console.log(calculation);
+//console.log(calculation + 2);
+
+//let result = calculation + 2;
+//console.log(result);
+
+//let message = 'good morning';
+//console.log(message);
+
+//variable2  = 5;
+//console.log(variable2);
+
+//variable2 = variable2 + 8;
+//console.log( variable2);
+
+//let phoneQuantity = 0;
+//const variable4 = 4;
+//var variable6 = 8;
+//console.log(typeof variable4);
